3c1e63b039cc3679fd3b1540a90716d638d5e3ed
MRw-Overlord <artem.gordievich.02@mail.ru>
2023-11-29 19:28:27 +0300

    initial commit

---
+++ src/test/java/de/bergmann/runningtracker/RunningTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bergmann.runningtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
